ad1156ae4c14c515c8990560c4db3c4c36237d0e
usertest-thom <thom.maurick.r@gmail.com>
2021-05-30 00:22:29 -0500

    creacion de modelos

---
+++ app/src/main/java/com/android/publisherapp/models/Producto.kt
@@ -0,0 +1,3 @@
+package com.android.publisherapp.models
+
+data class Producto(val nombre: String, val tipo: String,val image:String,val stock:String)
---
+++ app/src/main/java/com/android/publisherapp/models/Usuario.kt
@@ -0,0 +1,3 @@
+package com.android.publisherapp.models
+
+data class Usuario(val nombre: String, val edad: Int,val email:String,val dni:String,val foto:String)
