c1ce95127901f90f6ddb5c043636494bcc751ed1
usertest-thom <thom.maurick.r@gmail.com>
2021-07-26 23:38:59 -0500

    T_T

--- app/src/main/java/com/android/publisherapp/views/UsuariosFragment.kt
+++ app/src/main/java/com/android/publisherapp/views/UsuariosFragment.kt
@@ -1,10 +1,13 @@
 package com.android.publisherapp.views
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.android.publisherapp.R
@@ -13,6 +16,7 @@ import com.android.publisherapp.adapters.UsuarioAdapter
 import com.android.publisherapp.models.Producto
 import com.android.publisherapp.models.Usuario
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
 
@@ -21,6 +25,11 @@ class UsuariosFragment : Fragment() {
 
     var recyclerUsuarios : RecyclerView? = null
     var usuariosAdapter:UsuarioAdapter?=null
+
+
+    var dialogBuilder: AlertDialog.Builder? = null
+    var dialog: AlertDialog? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -32,6 +41,26 @@ class UsuariosFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         recyclerUsuarios = view.findViewById(R.id.rv_usuarios)
+
+        var btnAddUsuario:FloatingActionButton = view.findViewById(R.id.btnAddUsuarios)
+
+        btnAddUsuario.setOnClickListener {
+            Log.i("TEST","click +")
+            dialogBuilder = AlertDialog.Builder(context)
+            var viewUsuario:View = layoutInflater.inflate(R.layout.usuarios_add,null)
+            dialogBuilder?.setView(viewUsuario)
+            dialog = dialogBuilder?.create()
+            dialog?.setCancelable(false)
+            dialog?.show()
+
+
+            var btnAdd =viewUsuario.findViewById<Button>(R.id.btnAddUsuarios)
+            var btnCancelar = viewUsuario.findViewById<Button>(R.id.btnCancelarUser)
+
+            btnCancelar.setOnClickListener {
+                dialog?.dismiss()
+            }
+        }
 
         recyclerUsuarios?.layoutManager = LinearLayoutManager(activity)
         var query:Query = FirebaseFirestore.getInstance().collection("users")
